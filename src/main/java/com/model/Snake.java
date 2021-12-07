34edfb9d06390da188ba5786513c43fb1137b3e3
filippo.pilutti <filippo.pilutti@studio.unibo.it>
2021-12-07 16:49:51 +0100

    Complete tests for snake's model

--- src/main/java/com/model/Snake.java
+++ src/main/java/com/model/Snake.java
@@ -140,7 +140,6 @@ public final class Snake implements SnakeEntity {
 
     /** {@inheritDoc} */
     @Override
-    //TODO Test with JUnit.
     public Position nextPosition() {
         int x = this.getPosition().getX();
         int y = this.getPosition().getY();
@@ -189,21 +188,19 @@ public final class Snake implements SnakeEntity {
         // TODO JUnit test.
         // Check if the next position would hit a wall or another body part.
         if (this.body.contains(this.nextPosition())
-                || this.nextPosition().getX() <= 0 || this.nextPosition().getX() >= this.mapSizeX
-                || this.nextPosition().getY() <= 0 || this.nextPosition().getY() >= this.mapSizeY) {
+                || this.nextPosition().getX() < 0 || this.nextPosition().getX() > this.mapSizeX
+                || this.nextPosition().getY() < 0 || this.nextPosition().getY() > this.mapSizeY) {
             //this.dead = true;
             return;
         }
 
         /** Probably it has to be done like this. Add the new element on the head and remove the one on the tail.
-         *  TODO find a way to make sure that when snake eats an apple this move() method skips to remove the element on the tail,
-         *  using also the increaseLength().
          */
         this.headPosition = this.nextPosition();
         if (this.length == this.body.size()) {   // The length field should be increased when snake eats an apple,
-            this.body.remove(this.body.size());  // so we could use it to know when not to remove the element on the tail.
+            this.body.remove(this.body.size() - 1);  // so we could use it to know when not to remove the element on the tail.
         }
-        this.body.add(0, this.nextPosition());
+        this.body.add(0, this.headPosition);
 
     }
 
--- src/test/java/com/model/SnakeTest.java
+++ src/test/java/com/model/SnakeTest.java
@@ -2,6 +2,7 @@ package test.java.com.model;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -76,5 +77,83 @@ public class SnakeTest {
         snake.setDirection(Direction.DOWN);
         assertNotEquals(Direction.DOWN, snake.getDirection());
         assertEquals(Direction.UP, snake.getDirection());
+    }
+
+    @Test
+    @DisplayName("The next position should be computed correctly.")
+    public void testNextPosition() {
+        final SnakeEntity snake = new Snake.SnakeBuilder()
+                .direction(Direction.UP)
+                .headPosition(START_POS)
+                .body(new ArrayList<>())
+                .mapSize(X_MAP_SIZE, Y_MAP_SIZE)
+                .build();
+        assertEquals(new Pos(START_POS.getX(), START_POS.getY() - 1), snake.nextPosition());
+        snake.setDirection(Direction.RIGHT);
+        assertEquals(new Pos(START_POS.getX() + 1, START_POS.getY()), snake.nextPosition());
+        snake.setDirection(Direction.DOWN);
+        assertEquals(new Pos(START_POS.getX(), START_POS.getY() + 1), snake.nextPosition());
+        snake.setDirection(Direction.LEFT);
+        assertEquals(new Pos(START_POS.getX() - 1, START_POS.getY()), snake.nextPosition());
+    }
+
+    @Test
+    @DisplayName("Verify that the move() method works correctly.")
+    public void testMove() {
+        final List<Position> body = new ArrayList<>();
+        for (int i = 0; i < BODY_LENGTH; i++) {
+            body.add(new Pos(START_POS.getX(), START_POS.getY() + i));
+        }
+        final SnakeEntity snake = new Snake.SnakeBuilder()
+                                           .direction(Direction.UP)
+                                           .headPosition(START_POS)
+                                           .body(body)
+                                           .mapSize(X_MAP_SIZE, Y_MAP_SIZE)
+                                           .build();
+        snake.move();
+        assertEquals(new Pos(START_POS.getX(), START_POS.getY() - 1), snake.getPosition());
+        assertFalse(snake.getBodyPosition().contains(new Pos(START_POS.getX(), START_POS.getY() + BODY_LENGTH - 1)));
+        snake.move();
+        assertEquals(new Pos(START_POS.getX(), START_POS.getY() - 2), snake.getPosition());
+        assertFalse(snake.getBodyPosition().contains(new Pos(START_POS.getX(), START_POS.getY() + BODY_LENGTH - 2)));
+        snake.move();
+        snake.move();
+        snake.move();
+        snake.move();
+        snake.move();
+        snake.move();
+        snake.move();
+        snake.move();
+        assertEquals(new Pos(START_POS.getX(), 0), snake.getPosition());
+        snake.move(); // Here we try to move past the coordinate 0, the position should not change.
+        assertEquals(new Pos(START_POS.getX(), 0), snake.getPosition());
+        snake.setDirection(Direction.RIGHT);
+        snake.move();
+        assertEquals(new Pos(START_POS.getX() + 1, 0), snake.getPosition());
+        assertFalse(snake.getBodyPosition().contains(new Pos(START_POS.getX(), 4)));
+        snake.move();
+        assertEquals(new Pos(START_POS.getX() + 2, 0), snake.getPosition());
+        assertFalse(snake.getBodyPosition().contains(new Pos(START_POS.getX(), 3)));
+        // Now we try to simulate as if snake eats an apple.
+        snake.increaseLength();
+        snake.move();
+        assertEquals(new Pos(START_POS.getX() + 3, 0), snake.getPosition());
+        assertTrue(snake.getBodyPosition().contains(new Pos(START_POS.getX(), 2))); // The tail should not be removed now because the length was increased.
+        snake.move();
+        assertEquals(new Pos(START_POS.getX() + 4, 0), snake.getPosition());
+        assertFalse(snake.getBodyPosition().contains(new Pos(START_POS.getX(), 2)));
+        snake.move();
+        snake.move();
+        snake.move();
+        snake.move();
+        snake.move();
+        snake.move();
+        snake.move();
+        assertEquals(new Pos(X_MAP_SIZE, 0), snake.getPosition());
+        snake.move(); // Here we try to move past the map's size on the x coordinate, position should not change.
+        assertEquals(new Pos(X_MAP_SIZE, 0), snake.getPosition());
+        snake.setDirection(Direction.DOWN);
+        snake.move();
+        assertEquals(new Pos(X_MAP_SIZE, 1), snake.getPosition());
     }
 }
